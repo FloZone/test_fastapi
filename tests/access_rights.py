5987fe3c03e822f5fb416b6b4f1840251550ae73
FloZone <rod.florian@gmail.com>
2024-12-01 15:36:58 +0100

    ✅ add access rights tests

---
+++ tests/access_rights.py
@@ -0,0 +1,50 @@
+from enum import Enum, auto
+
+import pytest
+
+
+class Access(Enum):
+    OPEN = auto()
+    USER = auto()
+    ADMIN = auto()
+
+
+@pytest.mark.parametrize("fastapi_client", ("client", "client_user", "client_admin"))
+@pytest.mark.parametrize(
+    "method,url,access",
+    [
+        # Main endpoints
+        ("get", "/", Access.OPEN),
+        ("get", "/me", Access.USER),
+        # Users endpoints
+        ("post", "/users/", Access.OPEN),
+        ("get", "/users/", Access.USER),
+        ("get", "/users/1", Access.USER),
+        ("delete", "/users/1", Access.ADMIN),
+        # Resources endpoints
+        ("post", "/resources/", Access.ADMIN),
+        ("get", "/resources/", Access.USER),
+        ("get", "/resources/1", Access.USER),
+        ("delete", "/resources/1", Access.ADMIN),
+    ],
+)
+def test_access_rights(fastapi_client, method, url, access, request):
+    client = request.getfixturevalue(fastapi_client)
+    response = client.request(method, url)
+
+    if access == Access.OPEN:
+        assert response.status_code != 401
+    elif access == Access.USER:
+        if fastapi_client == "client":
+            assert response.status_code == 401
+        elif fastapi_client == "client_user":
+            assert response.status_code != 401
+        elif fastapi_client == "client_admin":
+            assert response.status_code != 401
+    elif access == Access.ADMIN:
+        if fastapi_client == "client":
+            assert response.status_code == 401
+        elif fastapi_client == "client_user":
+            assert response.status_code == 401
+        elif fastapi_client == "client_admin":
+            assert response.status_code != 401
